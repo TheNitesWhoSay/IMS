315425b973e5c5fa8d4b5e5590e6284f248493b0
jjf28 <forsbergjj@gmail.com>
2016-08-12 19:42:07 -0400

    Finished configuring & testing the database

--- ims-backend/src/main/java/com/revature/ims_backend/App.java
+++ ims-backend/src/main/java/com/revature/ims_backend/App.java
@@ -1,13 +1,16 @@
 package com.revature.ims_backend;
 
-/**
- * Hello world!
- *
- */
+import com.revature.beans.Classroom;
+import com.revature.persist.DataLayer;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	DataLayer dataLayer = new DataLayer();
+    	dataLayer.insertObject(new Classroom(1, 333, "Spire"));
+    	Classroom output = dataLayer.getClassroomById(1);
+    	System.out.println(output.getId() + " " + output.getRoomNumber() + " " + output.getBuilding());
+    	dataLayer.close();
     }
 }
