7ce897d32f9311e890d24607c72a81a75469f3bd
jjf28 <forsbergjj@gmail.com>
2016-08-14 21:00:58 -0400

    Order Beans
    Created the 'Order' beans and added many JPA annotations, some of which
    will need testing once combined with the 'Client' beans. A change was
    made to the database schema: PRODUCT_IMAGE(BLOB) was replaced with
    PRODUCT_IMAGE_ID(NUMBER) and a corresponding table was added
    IMS_PRODUCT_IMAGE{IMAGE_ID NUMBER, IMAGE_ADDRESS, VARCHAR2(2000)}. This
    table has been made to hold the address of images on the S3 server and
    potentially to hold extra information (like the caption/description for
    images that failed to load) in the future.

---
+++ ims-backend/src/main/java/com/revature/ims_backend/entities/OrderLine.java
@@ -0,0 +1,96 @@
+package com.revature.ims_backend.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="IMS_PO_LINE")
+public class OrderLine {
+	
+	@Id
+	@Column(name="ORDER_NUMBER")
+	private int orderNumber;
+	
+	@Column(name="LINE_NUMBER")
+	private int lineNumber;
+	
+	@Column(name="UNIT_PRICE")
+	private double unitPrice;
+	
+	@Column(name="QUANTITY_ORDERED")
+	private int quantityOrdered;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	private Product product;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private PurchaseOrder order; // Lazy-load
+	
+	
+	public OrderLine(int orderNumber, int lineNumber, double unitPrice, int quantityOrdered, Product product) {
+		super();
+		this.orderNumber = orderNumber;
+		this.lineNumber = lineNumber;
+		this.unitPrice = unitPrice;
+		this.quantityOrdered = quantityOrdered;
+		this.product = product;
+	}
+
+	public OrderLine() {
+		super();
+	}
+
+	public int getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(int orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public int getQuantityOrdered() {
+		return quantityOrdered;
+	}
+
+	public void setQuantityOrdered(int quantityOrdered) {
+		this.quantityOrdered = quantityOrdered;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public PurchaseOrder getOrder() {
+		return order;
+	}
+
+	public void setOrder(PurchaseOrder order) {
+		this.order = order;
+	}
+	
+	
+}
---
+++ ims-backend/src/main/java/com/revature/ims_backend/entities/Product.java
@@ -0,0 +1,178 @@
+package com.revature.ims_backend.entities;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="IMS_PRODUCT")
+public class Product {
+	
+	@Id
+	@Column(name="PRODUCT_UPC")
+	private int upc;
+	
+	@Column(name="PRODUCT_NAME")
+	private String name;
+	
+	@Column(name="PRODUCT_DESCRIPTION")
+	private String description;
+	
+	@Column(name="SHORT_NAME")
+	private String shortName;
+	
+	@Column(name="UNIT_COST")
+	private int unitCost;
+	
+	@Column(name="PACK_SIZE")
+	private String packSize;
+	
+	@Column(name="REORDER_QUANTITY")
+	private int reorderQuantity;
+	
+	@Column(name="RETAIL_PRICE")
+	private int retailPrice;
+	
+	@Column(name="PRODUCT_WEIGHT")
+	private int weight;
+	
+	@ManyToOne
+	@JoinColumn(name="PRODUCT_IMAGE_ID")
+	private ProductImage image;
+	
+	@ManyToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="categories")
+	private Set<Category> categories; // Lazy-load (probably)
+
+	
+	public Product(int upc, String name, String description, String shortName, int unitCost, String packSize,
+			int reorderQuantity, int retailPrice, int weight, ProductImage image, Set<Category> categories) {
+		super();
+		this.upc = upc;
+		this.name = name;
+		this.description = description;
+		this.shortName = shortName;
+		this.unitCost = unitCost;
+		this.packSize = packSize;
+		this.reorderQuantity = reorderQuantity;
+		this.retailPrice = retailPrice;
+		this.weight = weight;
+		this.image = image;
+		this.categories = categories;
+	}
+
+	public Product(int upc, String name, String description, String shortName, int unitCost, String packSize,
+			int reorderQuantity, int retailPrice, int weight, ProductImage image) {
+		super();
+		this.upc = upc;
+		this.name = name;
+		this.description = description;
+		this.shortName = shortName;
+		this.unitCost = unitCost;
+		this.packSize = packSize;
+		this.reorderQuantity = reorderQuantity;
+		this.retailPrice = retailPrice;
+		this.weight = weight;
+		this.image = image;
+	}
+
+	public Product() {
+		super();
+	}
+
+	public int getUpc() {
+		return upc;
+	}
+
+	public void setUpc(int upc) {
+		this.upc = upc;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	public int getUnitCost() {
+		return unitCost;
+	}
+
+	public void setUnitCost(int unitCost) {
+		this.unitCost = unitCost;
+	}
+
+	public String getPackSize() {
+		return packSize;
+	}
+
+	public void setPackSize(String packSize) {
+		this.packSize = packSize;
+	}
+
+	public int getReorderQuantity() {
+		return reorderQuantity;
+	}
+
+	public void setReorderQuantity(int reorderQuantity) {
+		this.reorderQuantity = reorderQuantity;
+	}
+
+	public int getRetailPrice() {
+		return retailPrice;
+	}
+
+	public void setRetailPrice(int retailPrice) {
+		this.retailPrice = retailPrice;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public ProductImage getImage() {
+		return image;
+	}
+
+	public void setImage(ProductImage image) {
+		this.image = image;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
+	
+}
