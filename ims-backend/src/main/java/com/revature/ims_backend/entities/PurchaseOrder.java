3ae1fc0917d06ed5036fe306b7f9e3476ff1b633
jjf28 <forsbergjj@gmail.com>
2016-08-15 22:58:55 -0400

    git ignore & metadata

--- ims-backend/src/main/java/com/revature/ims_backend/entities/PurchaseOrder.java
+++ ims-backend/src/main/java/com/revature/ims_backend/entities/PurchaseOrder.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.Criteria;
+
 @Entity
 @Table(name="IMS_PURCHASE_ORDER")
 public class PurchaseOrder {
